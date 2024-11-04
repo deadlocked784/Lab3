b12fb108b0aa344aefe713736c7cbbf52d430c15
Freddy <SEOWFREDDY.23@ichat.sp.edu.sg>
2024-11-04 08:49:20 +0800

    calculate average salary

--- employee_info.py
+++ employee_info.py
@@ -18,14 +18,17 @@ def get_employees_by_age_range(age_lower_limit, age_upper_limit):
 
     return result
 
+
 def calculate_average_salary():
     total = 0
-    average = 0
-
-    #add your implementation to calculate here
-
-
+    for item in employee_data:
+        print(f"Processing salary: {item['salary']}")
+        total += item["salary"]
+    average = total / len(employee_data) if employee_data else 0
     return average
+
+
+
 
 def get_employees_by_dept(department):
     result = []
